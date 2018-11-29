8ae21d74d0c7ed775845a42ae829e2303f1ff661
enderdude3108 <45468358+enderdude3108@users.noreply.github.com>
2018-11-29 19:56:10 +0000

    Update engine.py

--- project_python/engine.py
+++ project_python/engine.py
@@ -25,7 +25,7 @@ def main():
         libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         libtcod.console_flush()
 
-        libtcod.console_put_char(con,player_x,player_y,'',libtcod.BKGND_NONE)
+        libtcod.console_put_char(con,player_x,player_y,' ',libtcod.BKGND_NONE)
 
         action=handle_keys(key)
         move=action.get("move")
